6060c71d3a55ec382157cd834c592075ddb5d6c8
yufada <2450937592@qq.com>
2019-11-12 16:54:48 +0800

    logger-mocker

---
+++ dw-mock/src/main/java/com/atgla/log/dw/mock/RanOpt.java
@@ -0,0 +1,20 @@
+package com.atgla.log.dw.mock;
+
+public class RanOpt<T> {
+    T value ;
+    int weight;
+
+    public RanOpt ( T value, int weight ){
+        this.value=value ;
+        this.weight=weight;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
